0ebbc52579b581067721fcb2475b5b10329bc8eb
John Andrews <theunderdog09@gmail.com>
2020-02-14 23:06:33 -0700

    Sets are fuckin hard

--- coq/src/Untyped.v
+++ coq/src/Untyped.v
@@ -3,8 +3,10 @@
 Set Implicit Arguments.
 Set Asymmetric Patterns.
 
-Require Import Ensembles Finite_sets.
-Require Import Arith.
+Require Import Ensembles Finite_sets Finite_sets_facts Powerset Powerset_facts.
+Require Import Arith Omega.
+
+Require Import Tapl.Sets.
 
 Inductive term: Set :=
   | TTrue: term
@@ -23,7 +25,7 @@ Inductive consts: term -> Ensemble term :=
   | CPred: forall t con, consts t con -> consts (TPred t) con
   | CIsZero: forall t con, consts t con -> consts (TIsZero t) con
   | CIfThenElse: forall t1 t2 t3 con,
-      consts t1 con \/ consts t2 con \/ consts t3 con ->
+      In _ (Union _ (consts t1) (Union _ (consts t2) (consts t3))) con ->
       consts (TIfThenElse t1 t2 t3) con.
 
 Inductive size: term -> nat -> Prop :=
@@ -49,12 +51,6 @@ Inductive depth: term -> nat -> Prop :=
       depth (TIfThenElse t1 t2 t3) (S (max n1 (max n2 n3))).
 
 Hint Constructors term consts size depth: core.
-Hint Constructors cardinal: sets.
-
-Notation "'Yes'" := (left _).
-Notation "'No'" := (right _).
-Notation "'Reduce' x" := (if x then Yes else No) (at level 50).
-Notation "[ x ]" := (exist _ x _).
 
 Definition term_eq_dec: forall t1 t2: term, {t1 = t2} + {t1 <> t2}.
 Proof.
@@ -79,12 +75,111 @@ Proof.
 Qed.
 
 Definition consts_dec: forall t con, {consts t con} + {~ consts t con}.
-Abort.
+Proof.
+  refine (fix consts_dec t con :=
+    match t, con with
+    | TTrue, TTrue | TFalse, TFalse | TO, TO => Yes
+    | TSucc t', _ | TPred t', _ | TIsZero t', _ => Reduce (consts_dec t' con)
+    | TIfThenElse t1 t2 t3, _ =>
+        if consts_dec t1 con
+          then Yes
+          else if consts_dec t2 con
+            then Yes
+            else if consts_dec t3 con
+              then Yes
+              else No
+    | _, _ => No
+    end);
+    match goal with
+    | [ |- consts _ _ ] => constructor
+    | [ |- ~ consts _ _ ] => inversion 1
+    end; sets_auto.
+Defined.
 
-Ltac sig_auto :=
-  repeat match goal with
-  | [ H: { _ : _ | _ } |- _ ] => destruct H
-  end; auto.
+Inductive is_const: term -> Prop :=
+  | TrueConst: is_const TTrue
+  | FalseConst: is_const TFalse
+  | OConst: is_const TO.
+
+Hint Constructors is_const: core.
+
+Ltac decide_consts :=
+  intros; match goal with
+  | [ H: consts ?t ?con |- _ ] => destruct (consts_dec t con); inversion H
+  | [ H: In _ (consts ?t) ?con |- _ ] => destruct (consts_dec t con); inversion H
+  | [ H: is_const _ |- _ ] => inversion H
+  end; sets_auto.
+
+Lemma consts_subset: forall t, Included _ (consts t) (Triple _ TTrue TFalse TO).
+Proof.
+  induction t; sets_auto.
+
+  inversion H; sets_auto.
+  inversion H; sets_auto.
+  inversion H.
+Qed.
+
+Hint Resolve consts_subset: core.
+
+Lemma card_const_le_3: forall t n, cardinal _ (consts t) n -> n <= 3.
+Proof.
+  intros; specialize (@consts_subset t); eapply incl_card_le; sets_auto.
+  replace (Triple _ TTrue TFalse TO) with (Add _ (Add _ (Add _ (Empty_set _) TO) TFalse) TTrue).
+  constructor. constructor. constructor. constructor.
+  eauto with sets.
+  inversion 1; inversion H1.
+  inversion 1; inversion H1; inversion H3.
+  
+  sets_auto. inversion H0. inversion H1. inversion H3.
+  inversion H5.
+  inversion H5. apply Triple_r.
+  inversion H3. apply Triple_m.
+  inversion H1. apply Triple_l.
+
+  inversion H0.
+  apply Union_intror. apply In_singleton.
+  apply Union_introl, Union_intror. apply In_singleton.
+  do 2 apply Union_introl. apply Union_intror. apply In_singleton.
+Qed.
+
+Definition is_const_dec: forall t, {is_const t} + {~ is_const t}.
+Proof.
+  refine (fun t =>
+    if term_eq_dec t TTrue
+      then Yes
+      else if term_eq_dec t TFalse
+        then Yes
+        else if term_eq_dec t TO
+          then Yes
+          else No); subst;
+  match goal with
+  | [ |- is_const _ ] => constructor
+  | [ |- ~ is_const _ ] => inversion 1; congruence
+  end.
+Defined.
+
+Lemma no_nonconsts_in_consts: forall t t', In _ (consts t) t' -> is_const t'.
+Proof.
+  induction t; sets_auto; decide_consts.
+Qed.
+
+Hint Resolve consts_subset no_nonconsts_in_consts: core.
+
+Lemma card_consts_singleton: forall t, is_const t -> consts t = Singleton _ t.
+Proof.
+  inversion 1; sets_auto;
+  match goal with
+  | [ H: In _ (consts _) _ |- _ ] => inversion H
+  | [ H: In _ (Singleton _ _) _ |- _ ] => inversion H
+  end; sets_auto.
+Qed.
+
+Hint Resolve card_consts_singleton: core.
+
+Definition card_consts_dec: forall t, {n: nat | cardinal _ (consts t) n}.
+Proof.
+
+
 
 Definition size_dec: forall t, {n: nat | size t n}.
 Proof.
